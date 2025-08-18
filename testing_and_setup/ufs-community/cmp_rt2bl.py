0ecb95cd6aed3a2a5836a71ad6913388ee482363
dustinswales <dswales@ucar.edu>
2025-08-18 15:29:08 -0600

    Update CI script

--- testing_and_setup/ufs-community/cmp_rt2bl.py
+++ testing_and_setup/ufs-community/cmp_rt2bl.py
@@ -26,24 +26,57 @@ def parse_args():
     no_plots = args.no_plots
     return (dir_rt, dir_bl, no_plots)
 
-#
+def get_files(dirIN,prefix):
+    file_list = []
+    for root, dirs, files in os.walk(dirIN):
+        for file in files:
+            if file.endswith('.nc'):
+                if file.startswith(prefix):
+                    file_list.append(file)
+                # end if
+            # end if
+        # end for
+    # end for
+    return file_list
+# end def
+
+def compare_files(dir_bl, dir_rt, files):
+    file_bl = dir_bl+'/'+files
+    file_rt = dir_rt+'/'+files
+    if (os.path.isfile(file_rt)):
+        com = 'nccmp -d ' + file_bl + ' ' + file_rt
+        print('Comparing ',file_bl,' to ',file_rt)
+        os.system(com)
+        ierr = 0
+    else:
+        print("ERROR: Cannot find file for comparision, ",file_rt)
+        ierr = 1
+    # end if
+    return ierr
+# end def
+
 def main():
     #
     (dir_rt, dir_bl, no_plots) = parse_args()
 
     #
-    error_count = 0
     for run in run_list:
         print(run_list)
-        for root, dirs, files in os.walk(dir_rt):
-            for file in files:
-                if file.endswith('.nc'):
-                    print(file)
-                # end if
-            # end for
-        # end for
-    # end for
 
+        # MPAS history files (baselines)
+        file_bl_hist = get_files(dir_bl,'history.')
+
+        # MAPS diagnsotic files (baselines)
+        file_bl_diag = get_files(dir_bl,'diag.')
+
+        # Compare baselines to regression_test.
+        for files in file_bl_hist:
+            ierr = compare_files(dir_bl, dir_rt, files)
+        # end for
+
+        for files in file_bl_diag:
+            ierr = compare_files(dir_bl, dir_rt, files)
+        # end for
 #
 if __name__ == '__main__':
     main()
